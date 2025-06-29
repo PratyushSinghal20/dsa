4c2cb880b35738bd3d5554e036768fd7571808ea
PratyushSinghal20 <pratyush1820@gmail.com>
2025-06-29 13:11:12 +0530

    Add files via upload

---
+++ practice/5.cpp
@@ -0,0 +1,19 @@
+//array
+
+#include<iostream>
+using namespace std;
+
+int main() {
+// int arr[13];
+// cout << arr[3] ;
+
+// return 0;
+int arr[2] =  {1, 2};
+cout << arr[0] << endl;
+cout << arr[1] << endl;
+cout << arr[2] << endl;
+cout<< sizeof (arr) ;
+return 0;
+}
+
+
